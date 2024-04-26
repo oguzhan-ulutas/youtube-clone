4579667673862a485213c88e70d2aa2d92f2b31e
Oguzhan Ulutas <mkoulutas@gmail.com>
2024-04-26 17:50:27 +0300

    Add add video controller

---
+++ server/controllers/video.js
@@ -0,0 +1,15 @@
+export const addVideo = async (req, res, next) => {
+  const newVideo = new Video({ userId: req.user.id, ...req.body });
+  try {
+    const savedVideo = await newVideo.save();
+    res.status(200).json(savedVideo);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const updateVideo = async (req, res, next) => {};
+
+export const deleteVideo = async (req, res, next) => {};
+
+export const getVideo = async (req, res, next) => {};
--- server/routes/videos.js
+++ server/routes/videos.js
@@ -1,5 +1,18 @@
 import express from 'express';
+import { verifyToken } from '../verifyToken.js';
+import {
+  addVideo, updateVideo, deleteVideo, getVideo,
+} from '../controllers/video.js';
 
 const router = express.Router();
+
+// Create video
+router.post('/', verifyToken, addVideo);
+
+router.put('/:id', verifyToken, updateVideo);
+
+router.post('/:id', verifyToken, deleteVideo);
+
+router.post('/find/:id', getVideo);
 
 export default router;
