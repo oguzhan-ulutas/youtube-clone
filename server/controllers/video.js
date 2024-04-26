9aeacb75a450710b5bcde6ba05e29c0886a27770
Oguzhan Ulutas <mkoulutas@gmail.com>
2024-04-26 18:04:00 +0300

    Add update video controller

--- server/controllers/video.js
+++ server/controllers/video.js
@@ -1,3 +1,6 @@
+import { createError } from '../error.js';
+import Video from '../models/Video.js';
+
 export const addVideo = async (req, res, next) => {
   const newVideo = new Video({ userId: req.user.id, ...req.body });
   try {
@@ -8,8 +11,36 @@ export const addVideo = async (req, res, next) => {
   }
 };
 
-export const updateVideo = async (req, res, next) => {};
+export const updateVideo = async (req, res, next) => {
+  try {
+    const video = await Video.findById(req.params.id);
+    if (!video) return next(createError(404, 'Video can not be found!'));
 
-export const deleteVideo = async (req, res, next) => {};
+    if (req.user.id === video.userId) {
+      const updatedVideo = await Video.findByIdAndUpdate(req.params.id, {
+        $set: req.body,
+      }, { new: true });
+      res.status(200).json(updateVideo);
+    } else {
+      return next(createError(403, 'You can update only your videos!'));
+    }
+  } catch (error) {
+    next(error);
+  }
+};
 
-export const getVideo = async (req, res, next) => {};
+export const deleteVideo = async (req, res, next) => {
+  try {
+
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const getVideo = async (req, res, next) => {
+  try {
+
+  } catch (error) {
+    next(error);
+  }
+};
--- server/routes/videos.js
+++ server/routes/videos.js
@@ -9,10 +9,22 @@ const router = express.Router();
 // Create video
 router.post('/', verifyToken, addVideo);
 
+// Update video
 router.put('/:id', verifyToken, updateVideo);
 
+// Delete video
 router.post('/:id', verifyToken, deleteVideo);
 
+// Get video
 router.post('/find/:id', getVideo);
+
+// Increment view
+router.put('/view/id');
+
+// Get trend videos
+router.get('/trend');
+
+// Get random videos
+router.get('/random');
 
 export default router;
