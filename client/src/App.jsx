7635128f775c81da01bffc0e1c253e95d19b419a
Oguzhan Ulutas <mkoulutas@gmail.com>
2024-04-27 17:29:12 +0300

    Add search bar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,12 @@ import { useState } from 'react'
 import './App.css'
 import {BrowserRouter, Routes, Route} from "react-router-dom"
 import {Box} from "@mui/material"
+import Navbar from './components/Navbar'
+import Feed from './components/Feed'
+import VideoDetail from './components/VideoDetail'
+import ChannelDetail from './components/ChannelDetail'
+import SearchFeed from './components/SearchFeed'
+
 
 function App() {
   
@@ -9,9 +15,12 @@ function App() {
   return (
     <BrowserRouter>
       <Box sx={{backgroundColor:"#000"}}>
-        Navbar
+        <Navbar/>
         <Routes>
           <Route path='/' exact element={<Feed/>} />
+          <Route path='/video/:id' exact element={<VideoDetail/>} />
+          <Route path='/channel/:id' exact element={<ChannelDetail/>} />
+          <Route path='/search/:searchTerm' exact element={<SearchFeed/>} />
         </Routes>
 
       </Box>
